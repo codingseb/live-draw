2b1696db41aabbf91225ed1b9773c1e0c1859d1a
Sébastien Geiser <sgeiser@precitrame.com>
2022-10-19 11:18:06 +0200

    Color of the check when on White Color

--- LiveDraw/Persistence.cs
+++ LiveDraw/Persistence.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
